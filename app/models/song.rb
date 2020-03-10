26cee916cae5e929323bcd9ae65794342397cd1d
naveen thota <helloitsnaveen@gmail.com>
2020-03-10 11:08:57 -0700

    added basic AWS functionality, tested images > songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,9 @@
 class Song < ApplicationRecord
 
+    ### ACTIVE STORAGE ### 
+    has_one_attached :track 
+    has_one_attached :image
+
     ### MVP2 SONGS ###
     validates :title, :description, :artist_id, presence: true
     belongs_to :user, foreign_key: :artist_id, class_name: "User"
--- app/views/api/songs/show.json.jbuilder
+++ app/views/api/songs/show.json.jbuilder
@@ -1,1 +1,2 @@
 json.extract! @song, :id, :title, :description, :artist_id
+json.track url_for(@song.image)
