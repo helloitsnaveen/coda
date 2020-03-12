f3dadde0c899f68a71508785698f93dab03f8457
naveen thota <helloitsnaveen@gmail.com>
2020-03-12 00:05:44 -0700

    added working individual track audio player

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1697,7 +1697,11 @@ var SongIndexItem = function SongIndexItem(props) {
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
     fill: "currentColor",
     d: "M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"
-  }))))))));
+  }))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("audio", {
+    className: "songplayer",
+    controls: true,
+    src: props.song.track
+  }))));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (SongIndexItem);
--- frontend/components/song/song_index_item.jsx
+++ frontend/components/song/song_index_item.jsx
@@ -38,10 +38,17 @@ const SongIndexItem = props => (
                         <button className='song-index-item-play'>                        
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" className="play-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg>
                         </button>
+                        {/* <audio src={props.song.track}>PLAYER</audio> */}
+
                         {/* <img src={playIcon} className="play-icon" /> */}
                         {/* <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" className="play-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg> */}
                     </div>
                 </div>
+                <audio
+                    className='songplayer'
+                    controls
+                    src={props.song.track}>
+                </audio>
             </div>
         </div>
     </li>
