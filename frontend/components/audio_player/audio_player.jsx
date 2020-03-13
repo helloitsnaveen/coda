2adb2c761a1a82bdc6a6837f182a291c9c50071b
naveen thota <helloitsnaveen@gmail.com>
2020-03-13 09:44:12 -0700

    final commit pre-presentation

--- frontend/components/audio_player/audio_player.jsx
+++ frontend/components/audio_player/audio_player.jsx
@@ -26,6 +26,12 @@ class AudioPlayer extends React.Component {
             null
         ;
 
+        const showLink = this.props.currentSong.title ? 
+            <Link exact to={`/songs/${this.props.currentSong.id}`} ><svg className='AP-SHOW-ICON' aria-hidden="true" focusable="false" data-prefix="far" data-icon="dot-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 56c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m0-48C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 168c-44.183 0-80 35.817-80 80s35.817 80 80 80 80-35.817 80-80-35.817-80-80-80z"></path></svg></Link>
+            : 
+            <Link exact to={`/stream`} ><svg className='AP-SHOW-ICON' aria-hidden="true" focusable="false" data-prefix="far" data-icon="dot-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 56c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m0-48C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 168c-44.183 0-80 35.817-80 80s35.817 80 80 80 80-35.817 80-80-35.817-80-80-80z"></path></svg></Link>
+        ;
+
         return (
             <div className='AP-MUSIC-PLAYER-DIV'>
                 <div className='BORDER0'>
@@ -39,7 +45,7 @@ class AudioPlayer extends React.Component {
                         </div>
 
                         <div className='AP-show-container'>
-                            <Link exact to='' ><svg className='AP-SHOW-ICON' aria-hidden="true" focusable="false" data-prefix="far" data-icon="dot-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 56c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m0-48C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 168c-44.183 0-80 35.817-80 80s35.817 80 80 80 80-35.817 80-80-35.817-80-80-80z"></path></svg></Link>
+                            {showLink}
                         </div>
                     </div>
                 </div>
